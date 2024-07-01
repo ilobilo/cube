e10c40aa9b1f0e2b2f9ec9a862ed500bc9a05ec9
ilobilo <68286835+ilobilo@users.noreply.github.com>
2024-07-01 20:32:23 +0400

    switch to xmake

---
+++ xmake.lua
@@ -0,0 +1,16 @@
+add_rules("mode.release")
+add_rules("plugin.compile_commands.autoupdate", { outputdir = "build" })
+
+add_requires("glm")
+add_requires("ncursesw")
+
+target("cube")
+    set_kind("binary")
+    add_packages("glm", "ncursesw")
+
+    add_files("src/main.cpp", "src/cube.cpp")
+    add_includedirs("src/")
+
+    set_languages("c++20")
+    set_warnings("all", "error")
+    set_optimize("fastest")
